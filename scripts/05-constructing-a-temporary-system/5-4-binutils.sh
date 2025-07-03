68c5f6d799847ed29867021a23e48662075cc61a
Alvaro Vega Gomez <alvega-g@c3r8s6.42malaga.com>
2025-07-03 18:19:18 +0200

    outline for tarball scripts

---
+++ scripts/05-constructing-a-temporary-system/5-4-binutils.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Ensure user is lfs
+if ! whoami | grep -q "lfs"; then
+  echo "ERROR: Please become lfs: 'su - lfs'"
+  exit 1
+fi
+# Ensure $LFS is set
+if ! echo $LFS | grep -q "/mnt/lfs"; then
+  echo 'ERROR: Please set the "$LFS" variable before continuing'
+  exit 1
+fi
+
+# Variables
+tarball="binutils-2.32"
+
+########################
+# Generic build steps  #
+########################
+cd $LFS/sources
+tar -xvf $tarball.tar.xz
+cd $tarball
+
+########################
+# Specific build steps #
+########################
+mkdir -v build
+cd build
+
+../configure --prefix=/tools \
+  --with-sysroot=$LFS \
+  --with-lib-path=/tools/lib \
+  --target=$LFS_TGT \
+  --disable-nls \
+  --disable-werror
+
+make
+
+case $(uname -m) in
+x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
+esac
+
+make install
+
+#########################
+# Generic cleanup steps #
+#########################
+cd $LFS/sources
+rm -rf $tarball
