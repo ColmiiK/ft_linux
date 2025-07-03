2958d3842fa3feff3c56ccf52d8b952f20f4af5a
Alvaro Vega Gomez <alvega-g@c3r8s6.42malaga.com>
2025-07-03 14:26:34 +0200

    download packages

---
+++ scripts/03-packages-and-patches/3-1-sources-donwload.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create sources directory
+mkdir -v $LFS/sources
+
+# Ensure the directory is writable and sticky
+chmod -v a+wt $LFS/sources
+
+# Download curl if not installed
+if ! command -v curl >/dev/null 2>&1; then
+  echo "curl not found, installing..."
+  apt install curl
+fi
+
+# Get wget-list and md5sums
+curl https://www.linuxfromscratch.org/museum/lfs-museum/8.4/LFS-BOOK-8.4-HTML/wget-list >/tmp/wget-list
+curl https://www.linuxfromscratch.org/museum/lfs-museum/8.4/LFS-BOOK-8.4-HTML/md5sums >/tmp/md5sums
+
+# Download packages to sources
+wget --input-file=/tmp/wget-list --continue --directory-prefix=$LFS/sources
+
+# Check hashes of sources
+pushd $LFS/sources
+md5sum -c /tmp/md5sums
+popd
