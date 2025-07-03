bc98ccd9f61275e76d4dd72d434120763d7cee4d
Alvaro Vega Gomez <alvega-g@c3r8s6.42malaga.com>
2025-07-03 18:36:45 +0200

    add glibc

---
+++ scripts/05-constructing-a-temporary-system/5-7-glibc.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Ensure user is lfs
+if ! whoami | grep -q "lfs"; then
+  echo "ERROR: Please become lfs: 'su - lfs'"
+  exit 1
+fi
+# Ensure $LFS is set
+if ! echo $LFS | grep -q "/mnt/lfs"; then
+  echo 'ERROR: Please set the "$LFS" variable before continuing'
+  exit 1
+fi
+
+################
+# Tarball name #
+################
+tarball="glibc-2.29"
+
+########################
+# Generic build steps  #
+########################
+cd $LFS/sources
+tar -xvf $tarball.tar.xz
+if [ ! -d $LFS/sources/$tarball ]; then
+  echo "ERROR: Unable to extract tarball named $tarball, check the file extensions"
+  exit 1
+fi
+cd $tarball
+
+########################
+# Specific build steps #
+########################
+mkdir -v build
+cd build
+
+../configure \
+  --prefix=/tools \
+  --host=$LFS_TGT \
+  --build=$(../scripts/config.guess) \
+  --enable-kernel=3.2 \
+  --with-headers=/tools/include
+make
+make install
+
+echo 'int main(){}' >dummy.c
+$LFS_TGT-gcc dummy.c
+readelf -l a.out | grep ': /tools'
+rm -v dummy.c a.out
+
+#########################
+# Generic cleanup steps #
+#########################
+cd $LFS/sources
+rm -rf $tarball
