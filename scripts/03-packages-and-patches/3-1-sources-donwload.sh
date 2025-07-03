846b283fa30e51aa9a4abcf9495e9c3d8c330e15
Alvaro Vega Gomez <alvega-g@c3r8s6.42malaga.com>
2025-07-03 17:24:04 +0200

    fixed download script

--- scripts/03-packages-and-patches/3-1-sources-donwload.sh
+++ scripts/03-packages-and-patches/3-1-sources-donwload.sh
@@ -23,16 +23,27 @@ curl https://www.linuxfromscratch.org/museum/lfs-museum/8.4/LFS-BOOK-8.4-HTML/wg
 curl https://www.linuxfromscratch.org/museum/lfs-museum/8.4/LFS-BOOK-8.4-HTML/md5sums >/tmp/md5sums
 
 # Download packages to sources
-# wget --input-file=/tmp/wget-list --continue --directory-prefix=$LFS/sources
+wget --input-file=/tmp/wget-list --continue --directory-prefix=$LFS/sources
 
 # Download missing packages
-
-# Check for existing files first
-wget https://github.com/libexpat/libexpat/releases/download/R_2_2_6/expat-2.2.6.tar.bz2 --directory-prefix=$LFS/sources
-wget https://www.linuxfromscratch.org/museum/lfs-museum/8.4/lfs-bootscripts-20180820.tar.bz2 --directory-prefix=$LFS/sources
-wget https://www.kernel.org/pub/linux/docs/man-pages/Archive/man-pages-4.16.tar.xz --directory-prefix=$LFS/sources
-wget https://repository.timesys.com/buildsources/p/psmisc/psmisc-23.2/psmisc-23.2.tar.xz --directory-prefix=$LFS/sources
-wget https://zlib.net/fossils/zlib-1.2.11.tar.gz --directory-prefix=$LFS/sources
+if [ ! -f $LFS/sources/expat-2.2.6.tar.bz2 ]; then
+  wget https://github.com/libexpat/libexpat/releases/download/R_2_2_6/expat-2.2.6.tar.bz2 --directory-prefix=$LFS/sources
+fi
+if [ ! -f $LFS/sources/lfs-bootscripts-20180820.tar.bz2 ]; then
+  wget https://www.linuxfromscratch.org/museum/lfs-museum/8.4/lfs-bootscripts-20180820.tar.bz2 --directory-prefix=$LFS/sources
+fi
+if [ ! -f $LFS/sources/man-pages-4.16.tar.xz ]; then
+  wget https://www.kernel.org/pub/linux/docs/man-pages/Archive/man-pages-4.16.tar.xz --directory-prefix=$LFS/sources
+fi
+if [ ! -f $LFS/sources/psmisc-23.2.tar.xz ]; then
+  wget https://repository.timesys.com/buildsources/p/psmisc/psmisc-23.2/psmisc-23.2.tar.xz --directory-prefix=$LFS/sources
+fi
+if [ ! -f $LFS/sources/zlib-1.2.11.tar.gz ]; then
+  wget https://toolchains.bootlin.com/downloads/releases/sources/zlib-1.2.11/zlib-1.2.11.tar.xz --directory-prefix=$LFS/sources
+fi
+if [ ! -f $LFS/sources/vim-8.1.tar.bz2 ]; then
+  wget https://ftp.nluug.nl/pub/vim/unix/vim-8.1.tar.bz2 --directory-prefix=$LFS/sources
+fi
 
 # Check hashes of sources
 pushd $LFS/sources
