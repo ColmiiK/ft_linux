1e39eaaa2d4be30eec65ec460cc4809a0be5c743
Álvaro Vega Gómez <alvarvg@gmail.com>
2025-07-15 15:56:10 +0200

    bootscripts

---
+++ scripts/07-system-configuration/7.2-lfs-bootscripts.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# URL
+
+# Ensure chroot
+if [ "$(stat -c %d:%i /)" == "$(stat -c %d:%i /proc/1/root/.)" ]; then
+  echo "ERROR: Please enter chroot before continuing."
+  exit 1
+fi
+
+################
+# Tarball name #
+################
+tarball='lfs-bootscripts-20180820'
+
+########################
+# Generic build steps  #
+########################
+cd /sources
+echo "Extracting $tarball..."
+tar -xvf $tarball.tar.xz >/dev/null 2>&1
+tar -xvf $tarball.tar.gz >/dev/null 2>&1
+tar -xvf $tarball.tar.bz2 >/dev/null 2>&1
+if [ ! -d /sources/$tarball ]; then
+  echo "ERROR: Unable to extract tarball named $tarball"
+  exit 1
+fi
+echo "Extracted $tarball successfully"
+cd $tarball
+
+########################
+# Specific build steps #
+########################
+make install
+
+#########################
+# Generic cleanup steps #
+#########################
+cd /sources
+rm -rf $tarball
