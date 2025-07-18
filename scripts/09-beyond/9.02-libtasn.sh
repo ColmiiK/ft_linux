d63932e0f86f92628316d6e81c57bf9f2c759e61
Álvaro Vega Gómez <alvarvg@gmail.com>
2025-07-18 20:45:41 +0200

    libtasn done

---
+++ scripts/09-beyond/9.02-libtasn.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# URL https://www.linuxfromscratch.org/blfs/view/8.4/general/libtasn1.html
+
+# Ensure chroot
+if [ "$(stat -c %d:%i /)" == "$(stat -c %d:%i /proc/1/root/.)" ]; then
+  echo "ERROR: Please enter chroot before continuing."
+  exit 1
+fi
+
+################
+# Tarball name #
+################
+tarball='libtasn1-4.13'
+
+########################
+# Generic build steps  #
+########################
+cd /sources
+echo "Extracting $tarball..."
+tar -xvf $tarball.tar.xz >/dev/null 2>&1
+tar -xvf $tarball.tar.gz >/dev/null 2>&1
+tar -xvf $tarball.tar.bz2 >/dev/null 2>&1
+if [ ! -d /sources/$tarball ]; then
+  echo "ERROR: Unable to extract tarball named $tarball"
+  exit 1
+fi
+echo "Extracted $tarball successfully"
+cd $tarball
+
+########################
+# Specific build steps #
+########################
+./configure --prefix=/usr --disable-static &&
+  make
+make install
+make -C doc/reference install-data-local
+
+#########################
+# Generic cleanup steps #
+#########################
+cd /sources
+rm -rf $tarball
